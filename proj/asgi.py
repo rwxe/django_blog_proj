c3829aca1818538ce015a408788d0306e52506f6
yizdu <jzunk@protonmail.com>
2021-04-04 21:29:51 +0800

    refactor(global):重构项目名称为proj

--- proj/asgi.py
+++ proj/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for bhcsc project.
+ASGI config for proj project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bhcsc.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
 
 application = get_asgi_application()
